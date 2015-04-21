328f525830dc9d2e96be48ed1f1496a55ab612bc
erlendw <erlendwestbye@gmail.com>
2015-04-21 20:02:27 +0200

    Client husker om den har vært kjørt på maskinen før, om ikke opretter den en fil med ip, og iden den mottar fra server

--- udpClient.py
+++ udpClient.py
@@ -9,6 +9,7 @@ server_ip = '178.62.12.142'
 server_port = 5000
 
 
+
 def give_id():
 
     while True:
@@ -38,6 +39,7 @@ def give_id():
 
     return location
 
+
 def getLocalIp():
 
     computer_name = socket.gethostname()
@@ -50,21 +52,36 @@ def getLocalIp():
     else:
         ip = list_of_ip[0]
 
-
     return ip
 
-def communicate_with_server():
 
-    first_start_up = '0'
+
+
+def testIp():
+
+    current_ip = getLocalIp()
 
     try:
 
-        location_id = pickle.load(open('id.p','rb'))
-        first_start_up ='1'
+        old_ip = pickle.load(open('ip.p','rb'))
 
     except Exception as e:
 
-        location_id = give_id()
+        old_ip = ''
+
+
+    if(old_ip != current_ip):
+
+        pickle.dump(current_ip,open('ip.p', 'wb'))
+        return True
+
+    elif(old_ip == current_ip):
+
+        return False
+
+def communicate_with_server():
+
+    first_start_up = testIp()
 
     ip_host_of_client = getLocalIp()
     port_to_try = 5000 #will be auto assigned if occupied by another system
@@ -74,19 +91,26 @@ def communicate_with_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind((ip_host_of_client,port_to_try))
 
+    if(first_start_up):
+        location_id = give_id()
+        put_in_db = '0'
+    if not (first_start_up):
+        location_id = pickle.load(open('id.p','rb'))
+        put_in_db = '0'
     while True:
-        message = str.encode(location_id + ',' + first_start_up)
+
+        message = str.encode(location_id + ',' + put_in_db)
         s.sendto(message,server)
         print('Local ip is: ' + ip_host_of_client)
 
-        if(first_start_up == '0'):
+        if(first_start_up):
             location_id, addr = s.recvfrom(1024)
             location_id = bytes.decode(location_id)
             print(location_id)
             pickle.dump(location_id,open('id.p', 'wb'))
             print('server svarte med id: ' + location_id)
 
-        first_start_up = '1'
+        put_in_db = '1'
 
         zzz(3)
 
