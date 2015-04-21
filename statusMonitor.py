2ab8d17663c81fae4b02d3976a8c32fa1e1ab0bb
erlendw <erlendwestbye@gmail.com>
2015-04-21 21:38:24 +0200

    Påbegynt testing av status. Tanken er if(status == true) then start monitoring thread

--- statusMonitor.py
+++ statusMonitor.py
@@ -5,8 +5,8 @@ from time import sleep as zzz
 
 
 def confirm_status():
-
-    #if(statuschange)l
+    print('')
+    #if(statuschange)
 
     #i was pinged with this number
     #now i expect the same number within 5 seconds
