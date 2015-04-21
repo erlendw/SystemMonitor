b40782771896ccc494402a5bd3b30b889e1124bc
erlendw <erlendwestbye@gmail.com>
2015-04-21 09:54:42 +0200

    Begynt på DB og utbedret client

---
+++ db_read_write.py
@@ -0,0 +1,22 @@
+__author__ = 'erlend'
+
+
+#class variables
+
+host_of_db = ''
+user_log_in = ''
+pass_for_user = ''
+name_of_db = ''
+
+
+
+def connection():
+    print('')
+
+
+
+def sendTo():
+    print('')
+
+def recieveFrom():
+    print('')
--- udpClient.py
+++ udpClient.py
@@ -1,12 +1,30 @@
 import string
 __author__ = 'erlend'
 from time import sleep as zzz
-
 import socket
 
-def Main():
 
-    location = input('Where is this recorder located?')
+server_ip = '178.62.12.142'
+server_port = 5000
+
+
+def setLocation():
+
+    while True:
+        location = input('Where is this recorder located?')
+        confirmation = input('Please confirm')
+
+        if(str.upper(location) == str.upper(confirmation)):
+            if(str.upper(input('is the location ' + location + " correct? Y/N")) == 'Y'):
+                break
+            else:
+                continue
+
+        print('\nPlease make sure your first and second input matches')
+
+    return location
+
+def getLocalIp():
 
     computer_name = socket.gethostname()
 
@@ -18,29 +36,32 @@ def Main():
     else:
         ip = list_of_ip[0]
 
-#    for i in range(0, len(list_of_ip)):
-#       if(list_of_ip[i].startswith('192')):
-#            ip = list_of_ip[i]
-#            break
 
-    host = ip
-    port = 5000
+    return ip
 
+def tellServerYouAreUp():
 
+    location = setLocation()
+    ip_host_of_client = getLocalIp()
+    port_to_try = 5000 #will be auto assigned if occupied by another system
 
-
-    server = ('178.62.12.142', 5000)
+    server = (server_ip, server_port) #ip of server
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind((host,port))
+    s.bind((ip_host_of_client,port_to_try))
 
     while True:
         message = str.encode(location)
         s.sendto(message,server)
-        print('Local ip is: ' + host)
+        print('Local ip is: ' + ip_host_of_client)
         zzz(3)
 
     s.close()
+
+def Main():
+
+    tellServerYouAreUp()
+
 
 if __name__ == '__main__':
     Main()
