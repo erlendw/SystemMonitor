fcad992f463cbdacb67d34c2d0081825bda13fa7
erlendw <erlendwestbye@gmail.com>
2015-04-24 11:11:56 +0200

    Bakend er nesten ferdig, begynt å se på user interface og koden som må inn der

---
+++ SystemMonitor.php
@@ -0,0 +1,41 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "hp36565a";
+
+
+
+
+$db_connection = mysql_connect($db_host,$db_user,$db_pass) or die ("could not connect");
+
+$selct_db = mysql_select_db('location_id') or die ('database not found');
+
+
+
+    if ($db_connection && $selct_db) {
+
+        $query = "SELECT * FROM  `assosiate_location_with_id`";
+
+        $fetch = mysql_query($query) or die ('fant ikke bruker');
+
+        while ($row = mysql_fetch_assoc($fetch)) {
+
+            echo "<br>";
+            echo "<br>";
+            echo "Lokasjon: ";
+            echo $row['location'];
+            echo "<br>";
+            echo "Id: ";
+            echo $row['id'];
+            echo "<br>";
+            echo "Status: ";
+            if ($row['status'] == '1') {
+                echo "OPPE";
+            } else {
+                echo "NEDE";
+            }
+        }
+
+}
+?>
